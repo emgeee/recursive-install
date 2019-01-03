d3528b53b5149793df23fae2d603d442197ca87b
Reinout Waelput <reinout.waelput@wolterskluwer.com>
2019-01-03 16:54:32 +0100

    Resolving code styling issues. Supporting production parameter. Resolving tests:  - no longer rely on node being the application associated with .js files on Windows.  - added a test for an invalid package-lock.json file. Upgraded dependencies and devDependencies to the latest versions.

--- recursive-ci.js
+++ recursive-ci.js
@@ -1,45 +1,50 @@
 #!/usr/bin/env node
 
-var path = require('path')
-var shell = require('shelljs')
-var execSync = require('child_process').execSync
-var argv = require('yargs').argv
+var path = require('path');
+var shell = require('shelljs');
+var execSync = require('child_process').execSync;
+var argv = require('yargs').argv;
 
-function noop (x) { return x }
+function noop (x) { return x; }
 
 function getPackageJsonLocations (dirname) {
   return shell.find(dirname)
     .filter(function (fname) {
       return !(fname.indexOf('node_modules') > -1 || fname[0] === '.') &&
-        path.basename(fname) === 'package.json'
+        path.basename(fname) === 'package-lock.json';
     })
     .map(function (fname) {
-      return path.dirname(fname)
-    })
+      return path.dirname(fname);
+    });
 }
 
 function npmCi (dir) {
   var exitCode = 0;
   try {
-    console.log('Installing ' + dir + '/package.json')
-    execSync('npm ci', { cwd: dir})
-    console.log('')
+    if(argv.production) {
+      console.log('Installing [ci] ' + dir + '/package-lock.json with --only=production option');
+      execSync('npm ci --only=production', { cwd: dir});
+    } else {
+      console.log('Installing [ci] ' + dir + '/package-lock.json');
+      execSync('npm ci', { cwd: dir});
+    }
+    console.log('');
   } catch (err) {
-    exitCode = err.status
+    exitCode = err.status;
   }
 
   return {
     dirname: dir,
     exitCode: exitCode
-  }
+  };
 }
 
 function filterRoot (dir) {
   if (path.normalize(dir) === path.normalize(process.cwd())) {
-    console.log('Skipping root package.json')
-    return false
+    console.log('Skipping root package-lock.json');
+    return false;
   } else {
-    return true
+    return true;
   }
 }
 
@@ -48,8 +53,8 @@ if (require.main === module) {
     .filter(argv.skipRoot ? filterRoot : noop)
     .map(npmCi)
     .reduce(function (code, result) {
-      return result.exitCode > code ? result.exitCode : code
-    }, 0)
+      return result.exitCode > code ? result.exitCode : code;
+    }, 0);
 
-  process.exit(exitCode)
+  process.exit(exitCode);
 }
